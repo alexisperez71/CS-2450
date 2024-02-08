2881d6df493a004afd408e4cfc13caf277f92e1f
danipeterson <112648333+danipeterson@users.noreply.github.com>
2024-02-08 14:25:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class VirtualMachine:
-    def __init__(self):
+    def __init__(self) -> object:
         self._memory = []
         self._accumulator = "0000"
-        with open("Test2.txt", "r") as file:
+        with open("Test1.txt", "r") as file:
             for line in file:
                 self._memory.append(line.strip("+").strip("\n"))
 
@@ -24,39 +24,37 @@ class VirtualMachine:
     
     def run(self):
         count = 0
-        while True:
-            curr = self._memory[count][0:2]
-
-            if curr == "43":
-                break
-            elif curr == "33":
-                self.multiply(self._memory[int(self._memory[count][2:4])])
-            elif curr == "32":
-                self.divide(self._memory[int(self._memory[count][2:4])])
-            elif curr == "40":
-                count = int(self._memory[count][2:4]) - 1
-            elif curr == "10":
-                self.read(curr)
-            elif curr == '11':
-                self.write(curr)
-            elif len(self._memory) == count:
-                raise IndexError("No More Executable Instructions")
-            
-            count += 1
-        print(self)
-
-        """
-        for i in range(0, len(self._memory)):
+        while(True)
             curr = self._memory[i][0:2]
             if curr == "43":
                 break
-            elif curr == "33":
-                self.multiply(self._memory[int(self._memory[i][2:4])])
+            elif curr == "10":
+                self.read(curr)
+            elif curr == "11":
+                self.write(curr)
+            elif curr == "20":
+                self.load(i)
+            elif curr == "21":
+                self.store(i)
+            elif curr == "30":
+                continue
+            elif curr == "31":
+                continue
             elif curr == "32":
-                self.divide(self._memory[int(self._memory[i][2:4])])
+                self.divide(self._memory[int(self._memory[count][2:4])])
+            elif curr == "33":
+                self.multiply(self._memory[int(self._memory[count][2:4])])
             elif curr == "40":
-                curr = 
-        print(self)"""
+                count = int(self._memory[count][2:4]) - 1
+            elif curr == "41":
+                continue
+            elif curr == "42":
+                continue
+            elif len(self._memory) == count:
+                raise IndexError("No More Executable Instructions")
+            count += 1
+
+        print(self)
 
     def read(self, curr): #Fischer
         """Triggered by instruction '10'. Reads a word from the keyboard in to a specific location in memory"""
@@ -67,20 +65,18 @@ class VirtualMachine:
             print(self._memory[mem_address])
         else:                                                           #otherwise, the command is calling an address that is > 100, which is outside the total memory. Raise an error;.
             raise IndexError("Segmentation fault. Memory address does not exist")
-    
 
-    def resize_memory(self, mem_address): #Fischer
+    def resize_memory(self, mem_address):  # Fischer
         """helper function to resize memory if needed"""
         new_list = [None] * mem_address
         for i in range(len(self._memory)):
-                new_list[i] = self._memory[i]
+            new_list[i] = self._memory[i]
         self._memory = new_list
-
 
     def write(self, curr):    #Fischer
         """Triggered by instruction '11'. Write a word from a specific location in memory to the screen"""
         mem_address = int(self._memory[curr][2:])
-        plus = "+" 
+        plus = "+"
         user_word = input("Enter a 4-digit command (Digits 1-9 only): ")
         new_word = plus + user_word
         if mem_address < 100 and mem_address >= len(self._memory):          #if writing to an address that is outside of the current memory, but less than 100, resize memory and write word
@@ -91,11 +87,12 @@ class VirtualMachine:
         else:
             raise IndexError("Segmentation fault. Cannot write to that memory address")
 
-    def load(self):
-        pass
+    def load(self, i):
+        self._accumulator = self._memory[int(self._memory[i][2:])]
 
-    def store(self):
-        pass
+
+    def store(self, i):
+        self._memory[int(self._memory[i][2:])] = self._accumulator
 
     def add(self):
         pass
@@ -114,6 +111,7 @@ class VirtualMachine:
             self._accumulator = "0" + self._accumulator
         if len(self.get_accumulator()) > 4:
             raise ValueError(f"Value Overflow; Accumlulator only supports up to 4 digits.")
+
 
 def main():
     cole = VirtualMachine()
